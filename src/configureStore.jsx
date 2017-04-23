5a3d36bf0a5425772456709d68dbf4d853bd7655
brooksbecton <broabect@ut.utm.edu>
2017-04-23 14:25:11 -0500

    Added line to use redux dev tools

--- src/configureStore.jsx
+++ src/configureStore.jsx
@@ -4,7 +4,8 @@ import reducers from './reducers'
 
 const configureStore = () => {
     return createStore(
-        reducers
+        reducers,
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     );
 }
 
