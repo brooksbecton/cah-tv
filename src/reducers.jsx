a9b7adf8dac8aa164dc212c952927f01f94baad1
brooksbecton <broabect@ut.utm.edu>
2017-04-23 18:45:05 -0500

    Fixed error in combine reducers file
    reducers.jsx was returning a function object instead of instantiating
    and returning the combined reducers :scream: :

--- src/reducers.jsx
+++ src/reducers.jsx
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import table from './CreateTable/reducers/table';
 
-const reducers = () => combineReducers({
+const reducers = combineReducers({
     table
 })
 
