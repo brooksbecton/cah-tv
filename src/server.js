09ad76c43362d38bab051f9ffcc1c02199f44a6c
brooksbecton <broabect@ut.utm.edu>
2017-04-24 19:32:51 -0500

    Added Boilerplate for API server

---
+++ src/server.js
@@ -0,0 +1,24 @@
+// server.js
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+var router = express.Router();
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function (req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+app.use('/api', router);
+
+// START THE SERVER
+app.listen(port);
+console.log('Magic happens on port ' + port);
