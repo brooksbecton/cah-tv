077ac698a5d075c237933b9757c46ff06ce64e92
brooksbecton <broabect@ut.utm.edu>
2017-04-26 22:19:55 -0500

    Added WebPack boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: './public/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})
+
+module.exports = {
+    entry: './src',
+    output: {
+        path: path.resolve('build'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    plugins: [HtmlWebpackPluginConfig]
+
+}
