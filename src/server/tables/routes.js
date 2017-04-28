39de572b229de6c03ea1a4e0386f6be8349f777c
brooksbecton <broabect@ut.utm.edu>
2017-04-27 19:40:46 -0500

    Corrected Responses Codes
    Changed the response codes from strings to numbers

--- src/server/tables/routes.js
+++ src/server/tables/routes.js
@@ -6,14 +6,14 @@ var table = require('./container');
 router.get('/', (req, res) => {
     table.getTables()
         .then(function(data) {
-            res.status('200').json(data.val());
+            res.status(200).json(data.val());
         })
 });
 
 router.post('/', (req, res) => {
     table.createNewTable()
         .then(() => {
-            res.status('200').send('Success');
+            res.status(200).send('Success');
         })
 });
 
@@ -21,9 +21,9 @@ router.get('/:id', (req, res) => {
     var id = req.params.id;
     table.getTable(id).then((data) => {
         if (data !== null) {
-            res.status('200').json(data.val());
+            res.status(200).json(data.val());
         } else {
-            res.status('404').send('Table not found');
+            res.status(404).send('Table not found');
         }
     })
 });
@@ -32,9 +32,9 @@ router.delete('/:id', (req, res) => {
     var id = req.params.id;
     table.deleteTable(id).then(() => {
         if (data !== null) {
-            res.status('200').send('success');
+            res.status(200).send('success');
         } else {
-            res.status('404').send('Table not found');
+            res.status(404).send('Table not found');
         }
     })
 });
