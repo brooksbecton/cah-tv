786126671a1482dbc3d23463868c1e176b55ee99
brooksbecton <broabect@ut.utm.edu>
2017-05-04 10:38:44 -0500

    Added endpoint for getting card ids back
    This end point can take in an array of deck names and return an object
    that has all the ids in one from each deck supplied.

--- src/server/cards/container.js
+++ src/server/cards/container.js
@@ -1,7 +1,25 @@
 const cards = require('./../assets/allCards.json');
 
 module.exports = {
+
+    //Returns all card data
     getAll: () => {
         return cards;
+    },
+
+    /**
+     * Takes in an array of deck names 
+     * Returns: obj with white and black card id's
+     *         {'black': [1,2,3,...], 'white': [4,5,6,...]}
+     */
+    initCardIds: (deckNames) => {
+        let totalCards = { 'black': [], 'white': [] }
+
+        deckNames.map((name) => {
+            let deck = cards[name];
+            totalCards['black'] = totalCards['black'].concat(deck['black'])
+            totalCards['white'] = totalCards['white'].concat(deck['white'])
+        });
+        return totalCards;
     }
 }
--- src/server/cards/routes.js
+++ src/server/cards/routes.js
@@ -1,10 +1,23 @@
+const bodyParser = require('body-parser')
 const express = require('express');
 const router = express.Router();
+router.use(bodyParser.urlencoded({ extended: true }));
 
 const cards = require('./container');
 
 router.get('/', (req, res) => {
     res.json(cards.getAll());
+});
+
+//Returns Ids for each deck received 
+router.post('/decks/', (req, res) => {
+
+    /**
+     * Expects "decks" in body to be sent as array
+     * e.g. - decks: ["Base", "CAHe1"]
+     */
+    const decks = JSON.parse(req.body.decks);
+    res.json(cards.initCardIds(decks));
 });
 
 module.exports = router
